b527a1f175073dc93c4be6f5079abbb7ab42ce28
Mike Schwall <mikschwall@gmail.com>
2025-04-25 11:02:22 -0500

    adding queryclient to test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 describe('App Component', () => {
+  beforeEach(() => {
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve([{ title: 'delectus aut autem' }]),
+      })
+    );
+  });
+
   it('renders the text from api', async () => {
-    render(<App />);
-   await expect(screen.getByText(/delectus aut autem/i)).toBeInTheDocument();
+    const queryClient = new QueryClient();  // Create a query client instance
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    );
+
+    expect(await screen.findByText(/delectus aut autem/i)).toBeInTheDocument();
   });
 });
